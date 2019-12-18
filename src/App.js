1f8e7f7e6459e2a6283ae551a0fa9b154523fdd6
Yash Nalla <yashnalla98@gmail.com>
2019-12-18 17:03:21 -0500

    added axios request

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { Routes } from './Routes';
 import './App.scss';
+import axios from 'axios';
 
 class App extends Component {
 
@@ -12,6 +15,27 @@ class App extends Component {
         // TODO change this to your appname
         // TODO should the sample app webpack just rewrite this automatically?
         insights.chrome.identifyApp('fflag testing');
+
+        // axios.get('https://dog.ceo/api/breeds/image/random')
+        // .then(response => {
+        //     console.log(response.data);
+        // })
+        // .catch(error => {
+        //     console.log(error);
+        // });
+        let urlForHelloFlag = 'http://unleash-server-unleash-test.5a9f.insights-dev.openshiftapps.com/api/client/features/hello';
+        axios.get(urlForHelloFlag, {
+            headers: {
+                'Access-Control-Allow-Origin': 'http://unleash-server-unleash-test.5a9f.insights-dev.openshiftapps.com'
+                //'Content-Security-Policy': 'upgrade-insecure-requests'
+            }
+        })
+        .then(respone =>{
+            console.log(respone);
+        })
+        .catch(error =>{
+            console.log(error);
+        });
 
         this.appNav = insights.chrome.on('APP_NAVIGATION', event => this.props.history.push(`/${event.navId}`));
     }
